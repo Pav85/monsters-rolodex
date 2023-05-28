0b990c9574ff06471a7684bb1039804f1080bce1
Pav85 <pawel.werbowy@gmail.com>
2023-05-28 10:12:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,49 @@
 import { Component } from "react";
 
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
   constructor() {
+    console.log("constructor");
     super();
 
     this.state = {
-      monsters = [],
-    }
+      monsters: [],
+    };
   }
-
-  console.log('constructor')
 
   componentDidMount() {
-    console.log('componentDidMount');
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response) => response.json())
-    .then((users) => 
-    this.setState(
-      () => {
-        return { monsters: users };
-      },
-      () => {
-        console.log(this.state)
-      }
-    )
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div className="App">
+        <input />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
     );
   }
-};
-
+}
 
 export default App;
