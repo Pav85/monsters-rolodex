59da69e495a261723cffaacaed35c81b59149933
Pav85 <pawel.werbowy@gmail.com>
2023-06-06 10:44:18 +0100

    Update card-list.component.jsx

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,6 +2,7 @@ import { Component } from "react";
 
 class CardList extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1>Hello I'm a CardList component</h1>
