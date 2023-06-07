197859164b7b0ab620dd878dab89f6fa425838f5
Pav85 <pawel.werbowy@gmail.com>
2023-06-07 10:57:05 +0100

    Update card-list.component.jsx

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -10,13 +10,13 @@ class CardList extends Component {
         {monsters.map((monster) => {
           const { name, email, id } = monster;
           return (
-            <div className="card-container" key={monster.id}>
+            <div className="card-container" key={id}>
               <img
-                alt={`monster ${monster.name}`}
-                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
               />
-              <h2>{monster.name}</h2>
-              <p>{monster.email}</p>
+              <h2>{name}</h2>
+              <p>{email}</p>
             </div>
           );
         })}
