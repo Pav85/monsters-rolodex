ff4312261ae8a69d222ca755e3a0c3bb8a67ab42
Pav85 <pawel.werbowy@gmail.com>
2023-06-07 10:28:14 +0100

    Update search-box.compomet.jsx

--- src/components/search-box/search-box.compomet.jsx
+++ src/components/search-box/search-box.compomet.jsx
@@ -1,10 +1,12 @@
 import { Component } from "react";
 
+import "./search-box.styles.css";
+
 class SerachBox extends Component {
   render() {
     return (
       <input
-        className={`serach-box ${this.props.className}`}
+        className={`search-box ${this.props.className}`}
         type="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
